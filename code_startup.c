ed09b5c44c03ae12da76b244aabee307ee5af125
Anirudh-808 <aninagaraj808@gmail.com>
2025-05-27 10:27:02 +0530

    1st

---
+++ code_startup.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void check_file(FILE *file){
+    if (file = NULL)
+    {
+        printf("ERROR OPENING FILE!"); 
+        exit(1);
+    }
+}
+
+void copy_content(FILE *source, FILE *destination)
+{   
+    //used to copy content of file
+    char line[200];
+    while (fgets(line, sizeof(line) , source) != NULL)
+    {
+        fprintf(destination, "%s", line);
+    }
+}
+
+void check_ext(char *ext, FILE *real)
+{
+    // Used to check the extension of file
+    FILE *ref = NULL;
+    if (strcmp(ext, "txt") == 0) {
+        ref = fopen("ref.txt", "r");
+    } else if (strcmp(ext, "py") == 0) {
+        ref = fopen("ref.py", "r");
+    } else if (strcmp(ext, "c") == 0) {
+        ref = fopen("ref.c", "r");
+    } else {
+        printf("Unsupported file extension: %s\n", ext);
+        return; // Exit if the extension is not supported
+    }
+    check_file(ref);
+    copy_content(ref, real);
+    fclose(ref);
+}
+
+int main(int argc, char *argv[])
+{
+    //file pointers for actual file
+    FILE *actual;
+
+    char actual_file[100];
+    strcpy(actual_file, argv[1]);
+    printf("Actual file: %s\n", actual_file);
+
+    actual = fopen(actual_file , "w");
+    check_file(actual);
+    char actual_file_name[100];
+    char ext[10];
+
+    //trying to serparate the file name and extension
+    strcpy(actual_file_name , strtok(actual_file, "."));
+    strcpy(ext , strtok(NULL, "."));
+    printf("Extension: %s\n", ext);
+
+    check_ext(ext, actual);
+
+    fclose(actual);
+    return 0;
+}
